0a9dea991b21719a92657118232e18d2ce4fac8b
Clemens Büssow <clemens.buessow@gmx.de>
2016-11-15 10:12:07 +0100

    Sorted

--- WindparkSimulation/src/Main/Main.java
+++ WindparkSimulation/src/Main/Main.java
@@ -8,28 +8,29 @@ import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-Stage primaryStage;
-Pane region;
-Scene simulation;
-Group root;
+	Stage primaryStage;
+	Pane region;
+	Scene simulation;
+	Group root;
+
+	public static void main(String[] args) {
+		launch(args);
+	}
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		
+
 		this.primaryStage = primaryStage;
-		region = new  Pane();
+		region = new Pane();
 		root = new Group();
-		simulation = new Scene(root,1200,1000);
+		simulation = new Scene(root, 1200, 1000);
 		primaryStage.setScene(simulation);
 		simulation.setRoot(region);
-		
-		new GUI(simulation,region);
-		
+
+		new GUI(simulation, region);
+
 		primaryStage.show();
-		
-	}
-	public static void main (String[]args){
-		launch(args);
+
 	}
 
 }
